c8474d6d1dddda6c121c7d4d9a5348a016ffcab8
Stefan Ilie <stefan.ilie@hostmaker.co>
2020-02-06 18:05:04 +0200

    fix image url

--- src/utils.ts
+++ src/utils.ts
@@ -1,23 +1,29 @@
 import { get } from 'lodash';
-import { GiftedMessage, MessageFromPusher } from './interfaces';
+import { GiftedMessage, MessageFromPusher, MessagePart } from './interfaces';
 
-const getPayload = (message: MessageFromPusher) => get(
+const getPayload = (message: MessagePart) => get(
+  // check if type of payload is image, then do getImage
   message,
-  'parts[0].payload.content', 'Cannot render this message'
+  'payload.content', message.partType !== 'url' ? 'Cannot render this message' : ''
 );
 
-const getImage = (message: MessageFromPusher) => get(
+const getImage = (message: MessagePart) => get(
   message,
-  'parts[0].url'
+  'payload.url', ''
 );
 
 export function toGiftedChatMessage(message: MessageFromPusher): GiftedMessage {
+  const { parts = [] } = message;
+
+  const messageText = getPayload(parts[0]);
+  const messageImage = getImage(parts[0]);
+
   return {
     id: message.id,
     _id: message.id,
-    text: getPayload(message),
+    text: messageText,
     user: { ...message.sender, _id: message.sender.id },
     createdAt: new Date(message.createdAt),
-    image: getImage(message),
+    image: messageImage,
   };
 }
